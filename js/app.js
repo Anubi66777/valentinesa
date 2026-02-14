c67a1c5de895c06b2313ae3cf40644a4a48b4551
Anubi66777 <aztecsnake666@gmail.com>
2026-02-14 01:00:45 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,38 +6,36 @@ const vid = document.querySelector("video");
 let count = 2;
 
 const gifs = [
-  "../resources/cat-heart.gif",
-  "../resources/rusure.gif",
-  "../resources/3shocked-1.gif",
-  "../resources/4.crying.gif",
-  "../resources/5.crying.gif",
-  "../resources/idc.gif"
+  "resources/cat-heart.gif",
+  "resources/rusure.gif",
+  "resources/3shocked-1.gif",
+  "resources/4.crying.gif",
+  "resources/5.crying.gif",
+  "resources/idc.gif"
 ];
-
 
 gifs.forEach(gifSrc => {
   const img = new Image();
   img.src = gifSrc;
 });
 
-
 no.addEventListener("click", () => {
   if (count == 2) {
-    gif.src = "../resources/rusure.gif";
+    gif.src = "resources/rusure.gif";
     text.innerHTML = "You meant to press YES right?🤨";
     yes.style.height = "65%";
     yes.style.width = "60%";
     no.style.width = "30%";
     count++;
   } else if (count == 3) {
-    gif.src = "../resources/3shocked-1.gif";
+    gif.src = "resources/3shocked-1.gif";
     text.innerHTML = "Your hand must have slipped right?🥹";
     yes.style.height = "70%";
     yes.style.width = "70%";
     no.style.width = "20%";
     count++;
   } else if (count == 4) {
-    gif.src = "../resources/4.crying.gif";
+    gif.src = "resources/4.crying.gif";
     text.innerHTML = "I'm gonna cry😭";
     yes.style.height = "80%";
     yes.style.width = "80%";
@@ -45,7 +43,7 @@ no.addEventListener("click", () => {
     no.style.width = "10%";
     count++;
   } else if (count == 5) {
-    gif.src = "../resources/5.crying.gif";
+    gif.src = "resources/5.crying.gif";
     text.innerHTML = "BUBUUU Please🥺😘";
     yes.style.height = "90%";
     yes.style.width = "96%";
@@ -53,12 +51,11 @@ no.addEventListener("click", () => {
   }
 });
 
-
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/idc.gif";
+  gif.src = "resources/idc.gif";
   text.innerHTML = "Yayyyyyy 😘";
-  yes.innerHTML = '<a href="https://www.instagram.com/loop.codes/">Message me</a>';// inside the " " put your social media profile link
+  yes.innerHTML = '<a href="https://www.instagram.com/loop.codes/">Message me</a>';
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,3 +63,4 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
